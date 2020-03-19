169e0b08e41c05ab90c6e779efdcab1f14756c1f
Hudson McManus <hudson.mcmanus@gmail.com>
2020-03-19 03:31:40 -0700

    Added active btn feature, changed btn layout to qwerty

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+
+
 // JAVASCRIPT FILE //
 let lives = 10;
 
 // List of words and hints --> the hint and word have to be in same index
-let wordList = ["cindy", "hudson", "arjun", 'mrna', 'jumpsuit','Ygritte', 'khaleesi', 'redwedding'];
+let wordList = ["cindy", "hudson", "arjun", 'mrna', 'jumpsuit', 'Ygritte', 'khaleesi', 'redwedding'];
 let hintList = ["has a bio degree from UBC", "is youngest in program(?)", "has bio degree from uvic", 'a coding RNA that functions as the template for protein synthesis',
-                'the epic outfit Phoebe Waller-Bridge pulled off in Fleabag', '\'You know nothing Jon Snow.\'', 'a Dothraki word referring to the wife of the khal, or warlord of a khalasar, a Dothraki clan or tribe.',
-                'a massacre that takes place at the wedding that was intended to make peace between the Starks and the Freys'];
+    'the epic outfit Phoebe Waller-Bridge pulled off in Fleabag', '\'You know nothing Jon Snow.\'', 'a Dothraki word referring to the wife of the khal, or warlord of a khalasar, a Dothraki clan or tribe.',
+    'a massacre that takes place at the wedding that was intended to make peace between the Starks and the Freys'];
 
 // Decide which index/word onload  --> we gonna need to make it happen after a game is done too somehow
 let wordIndex = Math.floor(Math.random() * wordList.length);
@@ -17,7 +19,7 @@ let guessList = [];
 
 // display _ for each letter of selected word
 let blankWord = [];
-for (i = 0; i < secretWord.length; i++){
+for (i = 0; i < secretWord.length; i++) {
     blankWord[i] = "_"
 }
 
@@ -26,47 +28,85 @@ console.log(hint);
 console.log(blankWord);
 console.log(blankWord.join(" "));  // this is what should get written into the HTML 
 
+// Defining the keyboard wrapper
+let keyboard = document.getElementById('keyboard');
+
+// Creating buttons
+createButtons()
+
 
 function lifeReducer() {
     lives -= 1;
-    if (lives == 0){
+    if (lives == 0) {
         alert("Game over");
     }
 }
 
 function checkWonGame() {
     lettersLeft -= 1;
-    if (lettersLeft == 0){
+    if (lettersLeft == 0) {
         alert("You win");
     }
 }
 
-function guess(letter){
+function guess(btn) {
+    element = btn.target;
+    letter = element.innerHTML;
     counter = 0  // This is checking to see if there are any matches
 
-    if (guessList.includes(letter)){
-        alert("Already guessed.")
+    if (guessList.includes(letter)) {
+        alert("Already guessed.");
 
-    }else{
+    } else {
         guessList.push(letter);
         showGuesses();
 
-        for (i = 0; i < secretWord.length; i++){
-            if (secretWord[i] == letter){
+        for (i = 0; i < secretWord.length; i++) {
+            if (secretWord[i] == letter) {
                 blankWord[i] = letter;
                 counter += 1;
                 checkWonGame();
             }
         }
-        if (counter == 0){  // If no matches, reduce score by 1
+        if (counter == 0) {  // If no matches, reduce score by 1
             lifeReducer();
         }
     }
     console.log(counter)
     showLive();
     showLetterLeft();
-    showWord(); 
+    showWord();
     console.log(guessList)
+}
+
+
+function createButtons() {
+    // Create QWERTY array
+    let qwerty = "qwertyuiopasdfghjklzxcvbnm".split('');
+
+    console.log(keyboard)
+    for (let i = 0; i < qwerty.length; i++) {
+        if (i == 10 || i == 19) {
+            let linebreak = document.createElement("br");
+            keyboard.appendChild(linebreak)
+        }
+        // Create button
+        let btn = document.createElement("button")
+        // Assign button's text to it's cooresponding letter
+        btn.innerHTML = qwerty[i];
+        btn.className = 'btn';
+        // Add on click event to each button
+        btn.addEventListener('click', guess);
+        btn.addEventListener('click', function (btn) {
+                element = btn.target
+                element.className = 'btn active'
+        })
+        // Append it to the keyboard wrapper
+        keyboard.appendChild(btn);
+    }
+
+
+
 }
 
 // document.getElementById("keyA").onclick = guess("a");  // how do u make this not happen automatically lol
@@ -75,30 +115,30 @@ console.log(secretWord)
 console.log(blankWord.join(" "))  // this is what should get written into the HTML 
 
 // show hint
-function showHint(){
+function showHint() {
     document.getElementById('hintText').innerHTML = hint;
 }
 
 // show word
-function showWord(){
+function showWord() {
     document.getElementById('wordDisplay').innerHTML = blankWord.join(" ");
 }
 showWord();
 
 // show live
-function showLive(){
-    document.getElementById('live').innerHTML = 'Lives Remaining: '+ lives;
+function showLive() {
+    document.getElementById('live').innerHTML = 'Lives Remaining: ' + lives;
 }
 showLive();
 
 // show letter left
-function showLetterLeft(){
-    document.getElementById('letterLeft').innerHTML = 'Letters Remaining: '+ lettersLeft;
+function showLetterLeft() {
+    document.getElementById('letterLeft').innerHTML = 'Letters Remaining: ' + lettersLeft;
 }
 showLetterLeft();
 
 // show guesses
-function showGuesses(){
+function showGuesses() {
     guessList.sort()
-    document.getElementById('guesses').innerHTML = 'Guessed Letters: '+ guessList.join(" ");
+    document.getElementById('guesses').innerHTML = 'Guessed Letters: ' + guessList.join(" ");
 }
